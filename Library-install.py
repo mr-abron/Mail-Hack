cb6444f68e88b6bbff6cf5c8b4ccb14bc00e6612
Sardaruk Baloch <145233393+mr-abron@users.noreply.github.com>
2023-11-13 06:18:28 +0330

    Create Library-install.py

---
+++ Library-install.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+os.system("pip install smtplib")
+os.system("pip install random")
+os.system("pip install pyfiglet")
+os.system("pip install optparse")
+os.system("pip install time")
+os.system("pip install colorama")
+os.system("pio install sleep")
+os.system("pip install sys")
+os.system("python pasaword-saz.py")
